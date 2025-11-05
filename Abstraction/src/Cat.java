a8557a9e80953c9b804711fc0ecd58c97ff0e68d
dakdiu <dakkuethdiu@gmail.com>
2025-11-05 10:38:01 +0300

    Commit 2
    I added 1 private instance variable called sex on line 3 and created a constructor for it on line 8

--- Abstraction/src/Cat.java
+++ Abstraction/src/Cat.java
@@ -1,9 +1,11 @@
 public class Cat extends Animal {
     private String color;
+    private String sex;
 
     public Cat(String name, int age, String color) {
         super(name, age, color);
         this.color = color;
+        this.sex = sex;
     }
 
     @Override
