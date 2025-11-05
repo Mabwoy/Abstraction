9f22dc0001f8ce32a11988c45c4e9d950394f5fc
dakdiu <dakkuethdiu@gmail.com>
2025-11-05 08:43:32 +0300

    Commit 2
    In the new version on line 4, I updated the variables by adding one more protected variable and created a constructor for it

--- Abstraction/src/Animal.java
+++ Abstraction/src/Animal.java
@@ -1,10 +1,12 @@
 abstract class Animal {
     protected String name;
     protected int age;
+    protected String type;
 
-    public Animal(String name, int age) {
+    public Animal(String name, int age, String type) {
         this.name = name;
         this.age = age;
+        this.type = type;
     }
 
     public abstract void makeSound();
